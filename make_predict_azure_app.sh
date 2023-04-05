d06336087880dc834320a43638eb84749f02c56d
TechChefMM <mmendoza@mail.com>
2023-04-05 10:49:34 +0000

    update make_predict files - 202304050649. MM

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://udacity-training-flask-ml-service.azurewebsites.net:$PORT/predict 
-     # TODO: Replace the <yourappname> with your application name 
+     -X POST https://udactiy-training-flask-ml-service.azurewebsites.net:$PORT/predict
+     # TODO: Replace the <yourappname> with your application name
